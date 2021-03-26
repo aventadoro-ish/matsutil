49fe3699ca55c03dd0bc86855f4c57d34894c22c
aventadoro-ish <aventadoroqwerty@gmail.com>
2021-03-26 11:12:11 +0300

    refractor file

--- table.py
+++ table.py
@@ -56,16 +56,3 @@ class Table:
         return self.get_entry_by_column_id(column_idx, search_for)
 
 
-t = Table(['name', 'score'])
-t.__add__(['Mat', 1])
-t.__add__(['Valya', 2])
-t.__add__(['Tima', 3])
-t.__add__(['Ilya', 4])
-t.__add__(['Mat', 5])
-print(t.get_entry_by_column_name('name', 'Mat'))
-
-print(t.__len__())
-
-# print('\n'.join(dir(Table)))
-for i in t:
-    print(i[0])
